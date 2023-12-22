ba3e43f66fcecf1bd9511ffd79addd6517f9a6a0
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 21:21:47 +0300

    Create THEMES.js

---
+++ constants/THEMES.js
@@ -0,0 +1,12 @@
+
+export const lightTheme = {
+    background: '#FFFFFF',
+    foreground: '#000000',
+    text: '#000000',
+}
+
+export const darkTheme = {
+    background: '#000000',
+    foreground: '#FFFFFF',
+    text: '#FFFFFF',
+}
