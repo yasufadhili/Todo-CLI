fd79f688532829f7b5be35ccd105a8a5faf1f1be
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 20:47:06 +0300

    Update PaymentScreen.js

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -4,6 +4,6 @@ import { View } from 'react-native';
 
 export default function PaymentScreen() {
   const theme = useTheme();
-
+  
   return <View style={{ backgroundColor: theme.colors.primary, flex: 1 }} />;
 }
