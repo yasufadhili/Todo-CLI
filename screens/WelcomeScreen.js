ea985ed58696a0fb36723f80d0e6a7dc8a9a58c5
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 19:07:12 +0300

    Update WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { ThemeContext } from '../context/ThemeContext';
 
 export const WelcomeScreen = ({ navigation }) => {
 
-  const navigateDetails = () => {
+  const navigateSignin = () => {
     navigation.navigate('SigninScreen');
   };
 
+  const themeContext = useContext(ThemeContext);
+
+  const switchTheme = () => {
+    themeContext.toggleTheme
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TopNavigation title='MyApp' alignment='center'/>
-      <Divider/>
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+        <Button style={{ marginVertical: 4 }} onPress={navigateSignin}>OPEN Signin</Button>
+        <Button style={{ marginVertical: 4 }} onPress={switchTheme}>TOGGLE THEME</Button>
       </Layout>
     </SafeAreaView>
   );
