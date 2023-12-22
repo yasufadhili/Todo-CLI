9d2d14e7a1aad3c2db4150de4498e0a3bbe97b80
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 21:22:07 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
-
-import React, { useState } from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import {default as theme} from "./constants/COLORS.json";
-import { FeatherIconsPack } from './constants/feather-icons';
-import { MaterialIconsPack } from './constants/material-icons';
-import { IconRegistry } from '@ui-kitten/components';
-import Navigation from './navigation';
-import { ThemeContext } from './context/ThemeContext';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { PaperProvider } from 'react-native-paper';
-
+import React from 'react';
+import { ThemeProvider } from './context/ThemeContext';
+import ThemeSwitcher from './screens/ThemeSwitcher';
+import ExampleComponent from './components/ExampleComponent';
 
 const App = () => {
-
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    const nextTheme = theme === "dark" ? "light" : "dark";
-    setTheme(nextTheme);
-  }
-
-  return(
-    <>
-      <PaperProvider>
-        <IconRegistry icons={[FeatherIconsPack, MaterialIconsPack]} />
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-        <ApplicationProvider {...eva} theme={eva[theme]}>
-          <WelcomeScreen />
-        </ApplicationProvider>
-        </ThemeContext.Provider>
-      </PaperProvider>
-    </>
+  return (
+    <ThemeProvider>
+      
+    </ThemeProvider>
   );
 };
 
