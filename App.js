6c602d6e8286e941224416fedd74ba700bec0bb0
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 20:35:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import * as React from 'react';
-import { PaperProvider } from 'react-native-paper';
-import { Text } from 'react-native';
 
-export default function Main() {
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import PaymentScreen from './screens/PaymentScreen';
+
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'blue',
+    secondary: 'yellow',
+  },
+};
+
+export default function App() {
   return (
-    <PaperProvider>
-      <Text>Hello</Text>
+    <PaperProvider theme={theme}>
+      <PaymentScreen />
     </PaperProvider>
   );
 }
