c454d69dc7887e90d490b5e75c78c56ea7afaaa3
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 20:24:57 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,11 @@
-
-import React, { useState } from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import {default as theme} from "./constants/COLORS.json";
-import { FeatherIconsPack } from './constants/feather-icons';
-import { MaterialIconsPack } from './constants/material-icons';
-import { IconRegistry } from '@ui-kitten/components';
-import Navigation from './navigation';
-import { ThemeContext } from './context/ThemeContext';
-import WelcomeScreen from './screens/WelcomeScreen';
+import * as React from 'react';
 import { PaperProvider } from 'react-native-paper';
+import { Text } from 'react-native';
 
-
-const App = () => {
-
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    const nextTheme = theme === "dark" ? "light" : "dark";
-    setTheme(nextTheme);
-  }
-
-  return(
-    <>
-      <PaperProvider>
-        <IconRegistry icons={[FeatherIconsPack, MaterialIconsPack]} />
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-        <ApplicationProvider {...eva} theme={eva[theme]}>
-          <WelcomeScreen />
-        </ApplicationProvider>
-        </ThemeContext.Provider>
-      </PaperProvider>
-    </>
+export default function Main() {
+  return (
+    <PaperProvider>
+      <Text>Hello</Text>
+    </PaperProvider>
   );
-};
-
-export default App;
+}
