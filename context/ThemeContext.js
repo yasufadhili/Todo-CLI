c2e40faab76d6321826103a057708c7185f16e97
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 21:21:44 +0300

    Update ThemeContext.js

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { Appearance } from 'react-native';
+import lightTheme from '../constants/THEMES';
+import darkTheme from '../constants/THEMES';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const ThemeContext = React.createContext({
-  theme: 'dark',
-  toggleTheme: () => {},
-});
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const storeThemePreference = async (selectedTheme) => {
+    try {
+      await AsyncStorage.setItem('themePreference', selectedTheme);
+    } catch (error) {
+      console.error('Error storing theme preference:', error);
+    }
+  };
+
+  const getStoredThemePreference = async () => {
+    try {
+      const storedTheme = await AsyncStorage.getItem('themePreference');
+      if (storedTheme !== null) {
+        return storedTheme;
+      }
+      return Appearance.getColorScheme();
+    } catch (error) {
+      console.error('Error getting stored theme preference:', error);
+    }
+  };
+
+  useEffect(() => {
+    getStoredThemePreference().then((storedTheme) => {
+      const selectedTheme = storedTheme === 'dark' ? darkTheme : lightTheme;
+      setTheme(selectedTheme);
+    });
+  }, []);
+
+  const toggleTheme = (value) => {
+    const selectedTheme = value ? darkTheme : lightTheme;
+    setTheme(selectedTheme);
+    storeThemePreference(value ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
