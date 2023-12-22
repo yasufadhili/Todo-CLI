885e66df3b2f7f7930b60475c0f628486bc45571
Yasu Fadhili <132747121+Yasu-Fadhili@users.noreply.github.com>
2023-12-22 20:35:18 +0300

    Create PaymentScreen.js

---
+++ screens/PaymentScreen.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { useTheme } from 'react-native-paper';
+import { View } from 'react-native';
+
+export default function PaymentScreen() {
+  const theme = useTheme();
+
+  return <View style={{ backgroundColor: theme.colors.primary, flex: 1 }} />;
+}
